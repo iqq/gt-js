00a25e2636c77b13faa8b6d21981716e43109a54
Santiago Bonada <Santiago.Bonada@uoit.net>
2017-10-22 15:16:07 -0400

    Defined the creation of GazeTracker and GazeObjects

--- src/gazetracker.js
+++ src/gazetracker.js
@@ -1,4 +1,39 @@
+class GazeObject{
+	constructor(parent,element){
+		this.onCallbacks = new Map();
+		this.parent = parent;
+		this.element = element;
+	}
+
+	/* register a function to be called whenever the specified event in eventString happens */
+	on(eventString,callback){
+		this.onCallbacks.set(eventString,callback);
+		return this;
+	}
+
+	register(element){
+		return parent.register(element);
+	}
+}
+
 export default class GazeTracker {
-	constructor() {
+	constructor(){
+		this.gazeObjects = new Map()
+	}
+
+	register(element){
+		gazeObject = new GazeObject(this,element);
+		this.gazeObjects.set(element,gazeObject);
+
+		return gazeObject;
+	}
+
+	createGazeEvent(id, x, y){
+		var gazeEvent = new Object();
+		gazeEvent.id = id;
+		gazeEvent.x = x;
+		gazeEvent.y = y;
+
+		return gazeEvent;
 	}
 }
